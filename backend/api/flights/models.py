f75aa47e7be923c8ee7c99864759f864c7981662
lcirne <lcirne@uvm.edu>
2025-10-14 11:05:01 -0400

    Created a flights app in the API app for making calls to flights api and returning serialized json info to client. Created a basic flights model.

---
+++ backend/api/flights/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Flight(models.Model):
+    # Google Flights API fields
+    departure_id = models.CharField(max_length=3)
+    arrival_id = models.CharField(max_length=3)
+    gl = models.CharField(max_length=2)
+    hl = models.CharField(max_length=2)
+    type = models.IntegerField(default=1)
+    outbound_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    travel_class = models.IntegerField(blank=True, null=True)
+    exclude_basic = models.BooleanField(default=False)
+    deep_search = models.BooleanField(default=False)
+    # SerpAPI fields
+    engine = models.CharField(max_length=14, default="google_flights")
+    api_key = models.TextField("placeholder")
