715c257102ec6a24c4f91aa3b4e3ea6f13cda936
lcirne <lcirne@uvm.edu>
2025-10-16 10:23:35 -0400

    Changed naming convention of test client.

---
+++ routepages/test_clients/api_view_test.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def post_test(endpoint):
+    print("*"*30)
+    print("POST TEST")
+    print("*"*30)
+    get_response = requests.post(endpoint, json={
+                                    "engine": "google_flights",
+                                    "api_key": "placeholder",
+                                    "departure_id": "SFO",
+                                    "arrival_id": "SEA",
+                                    "gl": "us",
+                                    "hl": "en",
+                                    "type": 1,
+                                    "outbound_date": "2025-10-25T09:30:00Z",
+                                    "return_date": "2025-10-30T14:45:00Z",
+                                    "travel_class": 1,
+                                    "exclude_basic": False,
+                                    "deep_search": True
+                                    })
+
+    print(get_response.json())
+    print("")
+
+
+def get_test(endpoint):
+    print("*"*30)
+    print("GET TEST")
+    print("*"*30)
+    get_response = requests.get(endpoint)
+    print(get_response.json())
+    print("")
+
+
+if __name__ == "__main__":
+    endpoint = "http://127.0.0.1:8000/api/"
+    post_test(endpoint)
+    get_test(endpoint)
