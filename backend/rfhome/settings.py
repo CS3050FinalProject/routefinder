e0488133fc7fafd9dc31ec5e174f389f9f6d8e9b
lcirne <lcirne@uvm.edu>
2025-10-21 12:15:32 -0400

    Tree now up to date with main, django project accessible through aws.

--- backend/rfhome/settings.py
+++ backend/rfhome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9s96h$j01u2n$0#+d7jfxi0b8r=oo2%mu&vrc13fem49m+4ii*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
