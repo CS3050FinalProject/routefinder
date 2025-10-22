0a522010ffaeb661d88c2ea6a06c286e14f76d18
lcirne <lcirne@uvm.edu>
2025-10-22 15:54:36 -0400

    Changed settings to work with AWS RDS PostgreSQL database.

--- backend/rfhome/settings.py
+++ backend/rfhome/settings.py
@@ -79,9 +79,12 @@ WSGI_APPLICATION = 'rfhome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('RDS_DB_NAME'),
+        'USER': os.environ.get('RDS_USERNAME'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': os.environ.get('RDS_HOSTNAME'),
+        'PORT': os.environ.get('RDS_PORT', '5432'),    }
 }
 
 
