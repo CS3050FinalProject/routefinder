aa9a278ecf19f1db724d49cbc2c47a5816ab1bd7
lcirne <lcirne@uvm.edu>
2025-10-27 11:01:27 -0400

    Trying to configure django settings to work with postgres.

--- backend/rfhome/settings.py
+++ backend/rfhome/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'rfhome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': os.environ.get('RDS_DB_NAME'),
-#        'USER': os.environ.get('RDS_USERNAME'),
-#        'PASSWORD': os.environ.get('RDS_PASSWORD'),
-#        'HOST': os.environ.get('RDS_HOSTNAME'),
-#        'PORT': os.environ.get('RDS_PORT', '5432'),    }
-#}
-
-# Use until DJango DB is configured
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('RDS_DB_NAME'),
+        'USER': os.environ.get('RDS_USERNAME'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': 'routefinder-db.c6lmku68y4of.us-east-1.rds.amazonaws.com',
+        'PORT': os.environ.get('RDS_PORT', '5432'),    }
 }
+
+# Use until DJango DB is configured
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
