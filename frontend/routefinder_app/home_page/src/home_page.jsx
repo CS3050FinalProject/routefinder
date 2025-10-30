3727a8fade6465ce433d4f33363fa9d6c991a4da
nmckew <143674991+nmckew@users.noreply.github.com>
2025-10-30 16:35:56 -0400

    changes to the css and added class names and took out the bullets Addtionally tried to add a transition and it does not fully work

--- frontend/routefinder_app/home_page/src/home_page.jsx
+++ frontend/routefinder_app/home_page/src/home_page.jsx
@@ -1,4 +1,4 @@
-//imports 
+//imports
 
 import React, { useState } from "react";
 import { Plane, TrainTrack, Rat, Redo, Split } from "lucide-react";
@@ -8,6 +8,7 @@ export default function AirportRoutes() {
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
   const [routes, setRoutes] = useState([]);
+  const [showRoutes, setShowRoutes] = useState(false);
 
   // Dummy route data. just call the function with the data we get from the api
   const dummyRoutes = [
@@ -20,16 +21,19 @@ export default function AirportRoutes() {
   // error handling on blank input
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (origin && destination) setRoutes(dummyRoutes);
+    if (origin && destination){
+      setShowRoutes(true);
+      setRoutes(dummyRoutes);
+    }
     else alert("Please enter both origin and destination airports.");
   };
 
-  
+
   {/* RouteCard
   A function that takes data about the trip (I forgot what data we were using but this is easily expanded)
-  returns a card containing all that data formatted horizontally  
+  returns a card containing all that data formatted horizontally
   I only return a a string of vehicleType rn because idk what images were putting here  */}
-  
+
   const RouteCard = ({ vehicleType, company, cost, layovers }) => {
   let IconComponent;
 
@@ -46,21 +50,22 @@ export default function AirportRoutes() {
       break;
   }
 
+  const classes = `route fade-in flex justify-center border border-gray-200 rounded-xl p-4 mb-3 bg-white shadow hover:shadow-md transition w-full max-w-3xl ${showRoutes ? "show" : ""}`;
+
   return (
-    <div className="flex justify-center border border-gray-200 rounded-xl p-4 mb-3 bg-white shadow hover:shadow-md transition w-full max-w-3xl">
-      <p className="text-gray-800 text-base flex items-center gap-2">
-        <IconComponent className="w-5 h-5 text-gray-600" />
-        <span className="font-semibold text-blue-700">
-           • {company}
-        </span>{" "}
-        •{" "}
-        <span className="font-medium">Cost = {cost}</span> • Layovers:{" "}
+    <div className={classes} >
+      {/*<p className="text-gray-800 text-base flex items-center gap-2">*/}
+      <div className="companyName font-semibold text-blue-700">
+        <IconComponent className="w-5 h-5 text-gray-600" /><p>{company}</p>
+        </div>
+        <p className="font-medium">Cost = {cost}</p>
+      <p>Layovers:{" "}</p>
         {layovers.length > 0 ? (
-          <span>[{layovers.join(", ")}]</span>
+          <p>[{layovers.join(", ")}]</p>
         ) : (
-          <span>Direct</span>
+          <p>Direct</p>
         )}
-      </p>
+      {/*</p>*/}
     </div>
   );
 };
@@ -103,9 +108,11 @@ export default function AirportRoutes() {
         {routes.length > 0 && (
           <div className="flex flex-col items-center mt-8 w-full">
             <h2 className="text-xl font-semibold mb-4">Available Routes:</h2>
+            <div className="routes">
             {routes.map((route) => (
               <RouteCard key={route.id} {...route} />
             ))}
+            </div>
           </div>
         )}
       </div>
