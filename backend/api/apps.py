63763b76e4351ae59567bb0fb9ccf2b912974fd6
lcirne <lcirne@uvm.edu>
2025-11-02 17:26:07 -0500

    Pylint changes.

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,8 +1,8 @@
-from django.apps import AppConfig
-
 """
 API apps
 """
+from django.apps import AppConfig
+
 class ApiConfig(AppConfig):
     """
     App config
--- backend/api/flights/apps.py
+++ backend/api/flights/apps.py
@@ -1,7 +1,7 @@
 """
 Flight apps.
 """
-#from django.apps import AppConfig
+from django.apps import AppConfig
 
 
 class FlightsConfig(AppConfig):
