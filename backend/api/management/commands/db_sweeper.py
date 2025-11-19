a7f4d285fb3745a755d1b97f0efef5a76a9a4781
lcirne <lcirne@uvm.edu>
2025-11-19 15:37:23 -0500

    Updating db sweeper time checks to use UTC standard.

--- backend/api/management/commands/db_sweeper.py
+++ backend/api/management/commands/db_sweeper.py
@@ -2,17 +2,16 @@
 Python script for cleaning database of old searches and flights.
 old: object created >= 1 hour ago
 """
-from time import sleep
+import datetime
 from api.searches.models import Search
 from django.core.management.base import BaseCommand
-from django.utils import timezone
 from datetime import timedelta
 
 class Command(BaseCommand):
     help = 'Deletes searches and related flights older than one hour'
 
     def handle(self, *args, **kwargs):
-        one_hour_ago = timezone.now() - timedelta(hours=1)
+        one_hour_ago = datetime.datetime.now() - timedelta(hours=1)
         old_searches = Search.objects.filter(search_datetime__lt=one_hour_ago)
 
         # Delete old searches
