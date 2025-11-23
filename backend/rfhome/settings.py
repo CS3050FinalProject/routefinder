f1728a3e604cb67035a0ce56472eb5c7117e1823
lukeholmes <Luke.Holmes@uvm.edu>
2025-11-23 18:15:05 -0500

    Add CORS support for frontend

--- backend/rfhome/settings.py
+++ backend/rfhome/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['.elasticbeanstalk.com', '.compute.amazonaws.com', 'routefinder
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +137,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://routefinder.lukeholmes.dev",  # add your production frontend domain
+]
