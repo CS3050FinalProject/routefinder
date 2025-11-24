3edc04092725969e45db81c652b620c72c40334a
lukeholmes <Luke.Holmes@uvm.edu>
2025-11-23 20:45:31 -0500

    allow amplify cors

--- backend/rfhome/settings.py
+++ backend/rfhome/settings.py
@@ -140,5 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://*.amplifyapp.com",
     "https://routefinder.lukeholmes.dev"
 ]
