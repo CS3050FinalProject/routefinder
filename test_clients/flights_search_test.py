e94763a502382d6b053f0b10c6e4995c768ea818
lcirne <lcirne@uvm.edu>
2025-11-29 17:06:40 -0500

    Modified test client endpoint.

--- test_clients/flights_search_test.py
+++ test_clients/flights_search_test.py
@@ -7,12 +7,12 @@ def get_test(endpoint):
     print("FLIGHTS SEARCH GET TEST")
     print("*"*30)
     get_response = requests.get(endpoint, params={
-        "departure_id": "MIA",
-        "arrival_id": "EWR",
+        "departure_id": "EWR",
+        "arrival_id": "ORD",
         #"gl": "us",
         "hl": "en",
         "type": 1,
-        "outbound_date": "2025-12-11",
+        "outbound_date": "2025-12-13",
         "return_date": "2025-12-16",
         "travel_class": 3,
         #"exclude_basic": False,
@@ -23,7 +23,8 @@ def get_test(endpoint):
 
 
 if __name__ == "__main__":
-    endpoint = "http://routefinder-https-env.us-east-1.elasticbeanstalk.com/flights/search/"
+    #endpoint = "http://routefinder-https-env.us-east-1.elasticbeanstalk.com/flights/search/"
+    endpoint = "https://routefinder.api.lukeholmes.dev/flights/search/"
     #endpoint = 'http://127.0.0.1:8000/flights/search'
     #post_test(endpoint)
     get_test(endpoint)
